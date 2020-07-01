d548d051caf86ee929cc6d971470cab1b298afb6
Nathan Downer <nathandowner123@gmail.com>
2020-07-01 14:12:52 -0500

    hide the menu

--- src/App.tsx
+++ src/App.tsx
@@ -30,9 +30,9 @@ const App: React.FC = () => {
 		<IonApp>
 			<IonReactRouter>
 				<IonSplitPane contentId="main">
-					<Menu />
+					{/* <Menu /> */}
 					<IonRouterOutlet id="main">
-						<Route path="/page/:name" component={Page} exact />
+						{/* <Route path="/page/:name" component={Page} exact /> */}
 						<Route path="/shots" component={Shots} exact />
 						<Redirect from="/" to="/shots" exact />
 					</IonRouterOutlet>
