3f9418ae8d1c218727c2a4461620bba85525181e
Nathan Downer <nathandowner123@gmail.com>
2020-07-01 14:14:09 -0500

    add deletion of players

--- src/components/PlayerList.tsx
+++ src/components/PlayerList.tsx
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Player from '../models/player.model';
-import { IonList, IonItem, IonLabel, IonListHeader } from '@ionic/react';
+import {
+	IonList,
+	IonItem,
+	IonLabel,
+	IonItemOptions,
+	IonIcon,
+	IonItemOption,
+	IonItemSliding,
+} from '@ionic/react';
+import { trash } from 'ionicons/icons';
 
 interface PlayerListProps {
 	players: Player[];
-	onClick: Function;
+	onClick(p: Player): any;
+	deletePlayer(p: Player): any;
 }
 
-const PlayerList: React.FC<PlayerListProps> = ({ players, onClick }) => {
+const PlayerList: React.FC<PlayerListProps> = ({
+	players,
+	onClick,
+	deletePlayer,
+}) => {
+	const slidingRef = useRef<HTMLIonItemSlidingElement>(null);
+	const listRef = useRef<HTMLIonListElement>(null);
+
+	const onDeletePlayer = async (p: Player) => {
+		listRef.current?.closeSlidingItems();
+		deletePlayer(p);
+	};
 	return (
-		<IonList>
-			<IonListHeader>
-				<IonLabel>Players</IonLabel>
-			</IonListHeader>
+		<IonList ref={listRef}>
 			{players.map((p, index) => (
-				<IonItem key={index} onClick={e => onClick(p)}>
-					<IonLabel>{p.name}</IonLabel>
-				</IonItem>
+				<IonItemSliding key={index} ref={slidingRef}>
+					<IonItem onClick={e => onClick(p)}>
+						<IonLabel>{p.name}</IonLabel>
+					</IonItem>
+					<IonItemOptions side="end">
+						<IonItemOption color="danger" onClick={() => onDeletePlayer(p)}>
+							<IonIcon slot="icon-only" icon={trash} />
+						</IonItemOption>
+					</IonItemOptions>
+				</IonItemSliding>
 			))}
 		</IonList>
 	);
--- src/pages/Shots.tsx
+++ src/pages/Shots.tsx
@@ -11,14 +11,15 @@ import {
 	IonModal,
 	IonButtons,
 	IonIcon,
+	IonList,
+	IonItem,
 } from '@ionic/react';
-
 import TennisCourt from '../components/TennisCourt';
 import AddPlayer from '../components/AddPlayer';
 import PlayerList from '../components/PlayerList';
 import GameSettingsModal from './GameSettingsModal';
 import { GameSettings } from '../models/gameSettings.model';
-import { settingsSharp } from 'ionicons/icons';
+import { settingsSharp, remove, play } from 'ionicons/icons';
 
 const playerList: Player[] = [
 	{
@@ -37,6 +38,11 @@ const playerList: Player[] = [
 		shotCount: 0,
 	},
 	{
+		name: 'Chad',
+		score: 0,
+		shotCount: 0,
+	},
+	{
 		name: 'Timmy',
 		score: 0,
 		shotCount: 0,
@@ -49,19 +55,45 @@ const Shots: React.FC = () => {
 	const [gameSettings, setGameSettings] = useState<GameSettings>(
 		new GameSettings()
 	);
+	const [hasGameStarted, setHasGameStarted] = useState(false);
 	const [showModal, setShowModal] = useState(false);
 
 	const onSelectPlayer = (player: Player) => {
-		if (currentPlayers.length < 4) {
+		if (currentPlayers.length < 4 && hasGameStarted) {
 			setCurrentPlayers([...currentPlayers, player]);
 			setPlayers(players.filter(p => p.name !== player.name));
 		}
 	};
 
-	const getShot = (player: Player) => {
+	const resetPlayerScores = () => {
+		let resetPlayers = [...currentPlayers];
+		resetPlayers.forEach(p => (p.score = 0));
+		setCurrentPlayers(resetPlayers);
+	};
+
+	const removeFromCourt = (player: Player) => {
 		player.score = 0;
-		setCurrentPlayers(currentPlayers.filter(cp => cp.name !== player.name));
-		setPlayers([...players, player]);
+		if (!gameSettings.rememberPoints) {
+			resetPlayerScores();
+		}
+		if (gameSettings.changePlayersAutomatically) {
+			if (players.length) {
+				setPlayers([...players].slice(1, players.length).concat(player));
+				setCurrentPlayers(
+					currentPlayers
+						.filter(cp => cp.name !== player.name)
+						.concat(players[0])
+				);
+			} else {
+				setPlayers([player]);
+				setCurrentPlayers(currentPlayers.filter(cp => cp.name !== player.name));
+			}
+		} else {
+			setCurrentPlayers([
+				...currentPlayers.filter(cp => cp.name !== player.name),
+			]);
+			setPlayers([...players, player]);
+		}
 	};
 
 	const addPlayer = (player: Player) => {
@@ -72,6 +104,9 @@ const Shots: React.FC = () => {
 		let newPlayers = [...currentPlayers];
 		newPlayers[index].score++;
 		setCurrentPlayers(newPlayers);
+		if (newPlayers[index].score === gameSettings.pointLimit) {
+			removeFromCourt(newPlayers[index]);
+		}
 		console.log(currentPlayers);
 	};
 
@@ -79,13 +114,41 @@ const Shots: React.FC = () => {
 		let newPlayers = [...currentPlayers];
 		let shotPlayer = newPlayers[index];
 		shotPlayer.shotCount++;
-		setCurrentPlayers(newPlayers);
-		getShot(shotPlayer);
+		// setCurrentPlayers(newPlayers);
+		removeFromCourt(shotPlayer);
 	};
 
 	const closeModal = (state: GameSettings) => {
 		setGameSettings(state);
 		setShowModal(false);
+	};
+
+	const deletePlayer = (player: Player) => {
+		setPlayers([...players].filter(p => p.name !== player.name));
+	};
+
+	const startGame = () => {
+		if (players.length >= 4) {
+			let startingList = players.slice(0, 4);
+			let whatsLeft = players.slice(4, players.length);
+			setCurrentPlayers(startingList);
+			setPlayers(whatsLeft);
+		} else {
+			setCurrentPlayers([
+				...currentPlayers.concat(players.slice(0, players.length)),
+			]);
+			setPlayers([]);
+		}
+	};
+
+	const endGame = () => {
+		setPlayers([...players].concat(currentPlayers));
+		setCurrentPlayers([]);
+	};
+
+	const handleGameState = () => {
+		setHasGameStarted(!hasGameStarted);
+		hasGameStarted ? endGame() : startGame();
 	};
 
 	return (
@@ -101,17 +164,34 @@ const Shots: React.FC = () => {
 				</IonToolbar>
 			</IonHeader>
 			<IonContent className="ion-padding-horizontal">
+				<div className="background"></div>
 				<TennisCourt
 					currentPlayers={currentPlayers}
 					onMistake={addMistake}
 					onShot={addShot}
+					rememberShots={gameSettings.rememberShots}
 				/>
 				<AddPlayer addPlayer={addPlayer} />
-				<PlayerList players={players} onClick={onSelectPlayer} />
+				<PlayerList
+					players={players}
+					onClick={onSelectPlayer}
+					deletePlayer={deletePlayer}
+				/>
 				<IonModal isOpen={showModal}>
 					<GameSettingsModal onClose={closeModal} gameSettings={gameSettings} />
 				</IonModal>
 			</IonContent>
+			<IonFooter className="ion-padding">
+				<IonButton
+					expand="block"
+					fill={hasGameStarted ? 'solid' : 'outline'}
+					color={hasGameStarted ? 'danger' : 'solid'}
+					size="large"
+					onClick={handleGameState}
+				>
+					{hasGameStarted ? 'End Game' : 'Start Game'}
+				</IonButton>
+			</IonFooter>
 		</IonPage>
 	);
 };
